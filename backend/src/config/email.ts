920d332565fee50a29539d01f59d181e9c5fdbcf
faris-rahman-git <farisrahman786687@gmail.com>
2025-09-24 21:18:30 +0530

    fix: remove hardcoded SendGrid API key

--- backend/src/config/email.ts
+++ backend/src/config/email.ts
@@ -1,14 +1,29 @@
-import nodemailer from "nodemailer";
+import sgMail from "@sendgrid/mail";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.NODEMAILER_EMAIL,
-    pass: process.env.NODEMAILER_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false, 
-  },
-});
+sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
+
+export const sendMails = async (email: string, otp: string) => {
+  const msg = {
+    to: email,
+    from: process.env.SENDGRID_FROM_EMAIL!,
+    subject: "Readora OTP Verification",
+    html: `
+      <div style="font-family: Arial, sans-serif; line-height: 1.6; color: #333;">
+        <h2 style="color: #4CAF50;">🔐 OTP Verification</h2>
+        <p>Hello,</p>
+        <p>Your OTP is:</p>
+        <h1 style="background:#f4f4f4;padding:10px;display:inline-block;border-radius:5px;">
+          ${otp}
+        </h1>
+        <p>This code will expire in <b>2 minutes</b>.</p>
+        <p>If you didn’t request this, please ignore this email.</p>
+        <br>
+        <p>Thanks,<br>Readora Team</p>
+      </div>
+    `,
+  };
+
+  await sgMail.send(msg);
+};
--- backend/src/service/auth/otpService.ts
+++ backend/src/service/auth/otpService.ts
@@ -1,18 +1,12 @@
-import { transporter } from "../../config/email";
+import { sendMails } from "../../config/email";
 import otpModel from "../../model/otpModel";
+
 
 export const sendOtpEmail = async (
   email: string,
   otp: string
 ): Promise<void> => {
-  const mailOptions = {
-    from: process.env.NODEMAILER_EMAIL,
-    to: email,
-    subject: "Readora OTP Verification",
-    text: `Your OTP is ${otp}. It will expire in 2 minutes.`,
-  };
-
-  await transporter.sendMail(mailOptions);
+  sendMails(email, otp);
 };
 
 export const saveOtp = async (email: string, otp: string): Promise<void> => {
@@ -23,7 +17,10 @@ export const saveOtp = async (email: string, otp: string): Promise<void> => {
   );
 };
 
-export const isOtpValid = async (email: string, otp: string): Promise<boolean> => {
+export const isOtpValid = async (
+  email: string,
+  otp: string
+): Promise<boolean> => {
   const res = await otpModel.findOne({ email, otp });
   if (res) return true;
   return false;
